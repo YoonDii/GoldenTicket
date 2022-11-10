22d28135678fa463e3bd5e7edcda1986cdf128df
byeongjin <qudwls7468@gmail.com>
2022-11-10 10:18:45 +0900

    docs: templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
-app_name = 'accounts'
+app_name = "accounts"
 
 urlpatterns = [
-
+    path("", views.index, name="index"),
 ]
--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
-app_name = 'reviews'
+app_name = "reviews"
 
 urlpatterns = [
-
+    path("", views.index, name="index"),
 ]
