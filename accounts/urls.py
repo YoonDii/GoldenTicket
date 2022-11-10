735d50602814e2cd89b3a65a6315fa1c55c4c058
devhum7p <devhum7p@gmail.com>
2022-11-10 11:52:23 +0900

    feat: accounts/login & logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,6 @@ app_name = "accounts"
 urlpatterns = [
     path("", views.index, name="index"),
     path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
 ]
