cb9b44b67f00dbb461c7fc78bec8af444db90024
Bonbon315 <n202u123@gmail.com>
2022-11-10 10:33:24 +0900

    feat: created articles/model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+class Playlist(models.Model):
+    playid = models.CharField(max_length=20, primary_key=True)
+    playname = models.CharField(max_length=50)
+    genrename = models.CharField(max_length=20)
+    playstate = models.CharField(max_length=20)
+    playstdate = models.CharField(max_length=20)
+    playenddate = models.CharField(max_length=20)
+    poster = models.CharField(max_length=50)
+    locationname = models.CharField(max_length=20)
+
+
+class PlayDetail(models.Model):
+    playid = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    playname = models.CharField(max_length=50)
+    genrename = models.CharField(max_length=20)
+    playstate = models.CharField(max_length=20)
+    playstdate = models.CharField(max_length=20)
+    playenddate = models.CharField(max_length=20)
+    poster = models.CharField(max_length=20)
+    locationname = models.CharField(max_length=30)
+    playcast = models.CharField(max_length=50)
+    runtime = models.CharField(max_length=10)
+    age = models.CharField(max_length=10)
+    locationid = models.CharField(max_length=20)
+
+
+class PlayImage(models.Model):
+    playid = models.ForeignKey(PlayDetail, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="images/", blank=True)
+
+
+class LocationDetail(models.Model):
+    locationid = models.ForeignKey(PlayDetail, on_delete=models.CASCADE)
+    locationname = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    relateurl = models.CharField(max_length=50)
+    lat = models.CharField(max_length=20)
+    lgt = models.CharField(max_length=20)
