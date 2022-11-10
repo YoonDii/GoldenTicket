eb4f0863c47819f0bb496d719f1d53f68e109f2b
devhum7p <devhum7p@gmail.com>
2022-11-10 11:11:54 +0900

    feat : accounts/signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
+def index(request):
+
+    return render(request, "accounts/index.html")
+
+
+def signup(request):
+
+    if request.method == "POST":
+        sign_form = CustomUserCreationForm(request.POST)
+        if sign_form.is_valid():
+            user = sign_form.save()
+            auth_login(request, user)
+            return redirect("accounts:index")
+    else:
+        sign_form = CustomUserCreationForm()
+
+    context = {
+        "sign_form": sign_form,
+    }
+
+    return render(request, "accounts/signup.html", context)
