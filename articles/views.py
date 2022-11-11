af90426a364f786288c0fea520192747ecf9b87f
byeongjin <qudwls7468@gmail.com>
2022-11-11 11:23:24 +0900

    feat: main-variable

--- articles/views.py
+++ articles/views.py
@@ -1,15 +1,19 @@
-
 from django.shortcuts import render, redirect
 from .models import PlayDetail
 
 
 def main(request):
-    playlist = PlayDetail.objects.all()
+    play_list = PlayDetail.objects.filter(genrename="연극")
+    musical_list = PlayDetail.objects.filter(genrename="뮤지컬")
+    classic_list = PlayDetail.objects.filter(genrename="클래식")
     return render(
         request,
         "articles/main.html",
         {
-            "playlist": playlist[:1],
+            "playlist_rank": play_list[:3],
+            "playlist": play_list[:6],
+            "musical_list": musical_list[:6],
+            "classic_list": classic_list[:6],
         },
     )
 
