b5cc0bf684cca9dcd5c82652a505c8cb4237eee1
rhoeunbin <rhoeb21@gmail.com>
2022-11-11 11:25:37 +0900

    feat:social_login

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    kakao_id = models.BigIntegerField(null=True, unique=True)
+    naver_id = models.CharField(null=True, unique=True, max_length=100)
