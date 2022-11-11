8407736850884ecc47a4a440a8d83237e0b4a3f3
Bonbon315 <n202u123@gmail.com>
2022-11-11 17:38:26 +0900

    fix : delete concert url

--- articles/urls.py
+++ articles/urls.py
@@ -6,7 +6,6 @@ app_name = "articles"
 urlpatterns = [
     path("", views.main, name="main"),
     path("articles/", views.index, name="index"),
-    path("articles/concert/", views.concert, name="concert"),
     path("articles/<str:performance_pk>/", views.detail, name="detail"),
     path("articles/play/", views.play, name="play"),
 ]
