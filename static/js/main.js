21bf612825f126254a7117eaf7f4a6124e21bdf6
byeongjin <qudwls7468@gmail.com>
2022-11-14 09:59:53 +0900

    nothing

--- static/js/main.js
+++ static/js/main.js
@@ -17,3 +17,22 @@ buttons.forEach(button => {
     delete activeSlide.dataset.active
   })
 })
+
+
+// Ranking tag
+const tagList = document.querySelectorAll(".ranking__tag__name");
+const rankList = document.querySelectorAll(".ranking__list");
+let activeList = ''; // 활성화된 리스트를 담을 변수
+
+for (let i = 0; i < tagList.length; i++) {
+  tagList[i].querySelector('.tag-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    for (let j = 0; j < tagList.length; j++) {
+      tagList[j].classList.remove('on');
+      rankList[j].style.display = 'none';
+    }
+    this.parentNode.classList.add('on');
+    activeList = this.getAttribute('href');
+    document.querySelector(activeList).style.display = 'flex';
+  });
+}
