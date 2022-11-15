7712c0741e538287040fdcf234070429a12af9dc
devhum7p <devhum7p@gmail.com>
2022-11-15 09:33:41 +0900

    test: 인덱스 페이지 비동기 작업중

--- articles/urls.py
+++ articles/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     # path("articles/classic/", views.classic, name="classic"),
     # path("articles/dance/", views.dance, name="dance"),
     # path("articles/ktm/", views.ktm, name="ktm"),
+    path("articles/2/<str:genre>", views.index2, name="index2"),
 ]
