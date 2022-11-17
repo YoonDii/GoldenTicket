4488ebffa81bb643c8e0d41863ca18b499ef7ef3
byeongjin <qudwls7468@gmail.com>
2022-11-17 23:38:54 +0900

    feat: temp ranking

--- articles/urls.py
+++ articles/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("articles/", views.index, name="index"),
     path("articles/d/<str:performance_pk>/", views.detail, name="detail"),
     path("articles/<str:performance_pk>/like/", views.like, name="like"),
+    path("articles/ranking/", views.ranking, name="ranking"),
     # path("articles/play/", views.play, name="play"),
     # path("articles/musical/", views.musical, name="musical"),
     # path("articles/classic/", views.classic, name="classic"),
